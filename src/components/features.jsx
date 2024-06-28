e965cb8a1135da7f5c8cb5a2ae11ada885425bb7
krishna-sourcehub <133526957+krishna-sourcehub@users.noreply.github.com>
2024-06-29 03:31:03 +0530

    Update features.jsx

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -4,7 +4,7 @@ export const Features = (props) => {
   return (
     <div id="features" className="text-center">
       <div className="container">
-        <div className="col-md-10 col-md-offset-1 section-title">
+        <div className="col-md-101 col-md-offset-1 section-title">
     
           <h2 style={{marginTop:"100px"}}>Features</h2>
         </div>
